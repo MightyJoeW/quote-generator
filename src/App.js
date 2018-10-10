86a6a5b676c1391731c0db795ebb320e176ce4e9
Joe Warren <joe.warren7@gmail.com>
2018-10-09 23:44:19 -0500

    feature: creates quote and tweet functions with basic styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
+// EXTERNAL DEPENDENCIES
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
 
-class App extends Component {
+// LOCAL VARIABLES
+const url = 'https://talaikis.com/api/quotes/random/';
+
+const wrapper = {
+  height: '100vh',
+  textAlign: 'center',
+  width: '100vw'
+}
+
+const paperWrapper = {
+  alignItems: "center",
+  display: 'flex',
+  height: '100vh',
+  justifyContent: 'center'
+}
+
+const paper = {
+  padding: 15,
+  width: 500
+}
+
+// COMPONENT DEFINITION
+export default class App extends Component {
+  state = {
+    author: '',
+    quote: ''
+  }
+
+  getQuote = () => {
+    axios.get(url).then(res => {
+      this.setState({
+        author: res.data.author,
+        quote: res.data.quote
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.getQuote();
+  }
+
+  handleShuffle = () => {
+    this.getQuote();
+  }
+
+  handleTweet = () => {
+    console.log('%c tweet', 'color: #308e7b');
+  }
+
   render() {
+    const { author, quote } = this.state;
+    const populateTweet = `https://twitter.com/intent/tweet?text=${quote} -${author}`;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={wrapper}>
+        <CssBaseline />
+        <div style={paperWrapper}>
+          <Paper style={paper}>
+            <p>{quote ? quote : <CircularProgress />}</p>
+            <p>{author ? `- ${author}` : CircularProgress}</p>
+            <button type="button" onClick={this.handleShuffle}> New Quote </button>
+            <button type="button"> <a class="twitter-share-button" href={populateTweet}>Tweet Quote</a> </button>
+          </Paper>
+        </div>
       </div>
     );
   }
 }
-
-export default App;
